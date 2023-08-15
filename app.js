0527f9bda630ebdb92f1f162eee5074b72233f1e
QuantomErik <ey222ci@student.lnu.se>
2023-08-15 12:36:42 +0200

    Week_3

---
+++ app.js
@@ -0,0 +1,22 @@
+let firstName = 'Neo'
+const lastName = 'Yang'
+const city = 'Stockholm'
+let age = 18
+const occupation = 'student'
+
+if (typeof age === 'number') {
+    age = 1337
+}
+
+if (typeof firstName === 'string') {
+    firstName = 'Erik'
+}
+
+console.log('Hello my name is '+ firstName +', I am '+age+' years old.')
+
+if (occupation === 'student')
+if (city === 'göteborg') {
+    console.log(`I am a student at Lnu ${occupation} and living in ${göteborg}.`)
+}  else {
+    console.log(`I am a student at Lnu ${occupation} and living in ${city}.`)
+}
